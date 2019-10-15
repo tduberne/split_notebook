4f5f5e9bca68c524a0c0831a6db8f74eb1fda4e1
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2019-10-15 17:46:01 +0200

    allow to configure metadata more

--- generate_notebooks.py
+++ generate_notebooks.py
@@ -7,8 +7,16 @@ parser = OptionParser()
 parser.add_option('-s', '--solution', dest='solution_file',
                 help='solution output file name')
 
+parser.add_option('--solution-name', dest='solution_name',
+                default='solution',
+                help='value of the metadata for solution cells')
+
 parser.add_option('-e', '--exercise', dest='exercise_file',
                 help='exercise output file name')
+
+parser.add_option('--exercise-name', dest='exercise_name',
+                default='exercise',
+                help='value of the metadata for exercise cells')
 
 parser.add_option('-m', '--metadata', default='exercise_type',
                 help='name of metadata field')
@@ -18,6 +26,9 @@ options, args = parser.parse_args()
 original_notebook = jupytext.read(args[0])
 
 def _filter(nb, t):
+    if t is None:
+        raise ValueError('type was None')
+
     filtered = copy.deepcopy(nb)
     filtered['cells'] = [
         cell
@@ -31,6 +42,6 @@ def _filter_and_write(nb, t, out):
     if out:
         jupytext.write(_filter(nb, t), out)
 
-_filter_and_write(original_notebook, 'solution', options.solution_file)
-_filter_and_write(original_notebook, 'exercise', options.exercise_file)
+_filter_and_write(original_notebook, options.solution_name, options.solution_file)
+_filter_and_write(original_notebook, options.exercise_name, options.exercise_file)
 
